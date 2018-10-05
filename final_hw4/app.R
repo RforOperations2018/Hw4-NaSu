6bf30f96dbd360909e700b327a5b39f637a24940
sunasooner <31681877+sunasooner@users.noreply.github.com>
2018-10-05 19:28:47 -0400

    lable the plot

--- final_hw4/app.R
+++ final_hw4/app.R
@@ -22,11 +22,13 @@ ckanSQL <- function(url) {
   data.frame(jsonlite::fromJSON(json)$result$records)
 }
 
+#connect to API and get columns function
 ckanColumn <- function(id, field) {
   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20%22", field, "%22%20from%20%22", id, "%22")
   c(ckanSQL(URLencode(url)))
 }
 
+#get wanted columns to form dataframe
 departmenttype <- sort(ckanColumn("f61f6e8c-7b93-4df3-9935-4937899901c7", "department_name")$department_name)
 
 ledgercode <- sort(ckanColumn("f61f6e8c-7b93-4df3-9935-4937899901c7", "ledger_code")$ledger_code)
@@ -37,8 +39,8 @@ funddescription <- sort(ckanColumn("f61f6e8c-7b93-4df3-9935-4937899901c7", "fund
 
 data_all <- data.frame(departmenttype,ledgercode,amount_city,funddescription)
 
-# Define UI for applicatio
-ui <- navbarPage("Investing in Innovation Appplications NavBar", 
+# Define UI for application
+ui <- navbarPage("City Wide Revenues and Expenses NavBar", 
                  tabPanel("Plot",
                           sidebarLayout(
                             sidebarPanel(
@@ -54,7 +56,7 @@ ui <- navbarPage("Investing in Innovation Appplications NavBar",
                                                  "Ledgercode:",
                                                  choices = sort(unique(data_all$ledgercode)),
                                                  selected = c("4", "5")),
-                              # Award Requested Selection
+                              # Slider Selection
                               sliderInput("amount_select",
                                           "Amount:",
                                           min = min(data_all$amount_city, na.rm = T),
@@ -66,18 +68,18 @@ ui <- navbarPage("Investing in Innovation Appplications NavBar",
                             # Output plot
                             mainPanel(
                               fluidRow(
-                                plotlyOutput("plot_length")
+                                plotlyOutput("plot1")
                               ),
                               fluidRow(
-                                plotlyOutput("plot_requested")
+                                plotlyOutput("plot2")
                               ),
                               fluidRow(
-                                plotlyOutput("plot_requested2")
+                                plotlyOutput("plot3")
                               )))),
                  # Data Table
                  tabPanel("Table",
                           inputPanel(
-                            downloadButton("downloadData","Download Investing in Innovative Applications Data")
+                            downloadButton("downloadData","Download City Wide Revenues and Expenses Data")
                           ),
                           fluidPage(DT::dataTableOutput("table"))
                  )
@@ -87,58 +89,57 @@ ui <- navbarPage("Investing in Innovation Appplications NavBar",
 # Define server logic
 server <- function(input, output, session = session) {
   # Filtered investing data
-  iaInput <- reactive({
+  dataInput <- reactive({
     data <- data_all %>%
       # Slider Filter
       filter(amount_city >= input$amount_select[1] & amount_city <= input$amount_select[2])
-    # State Filter
+    #  Filter
     if (length(input$ledgercode_select) > 0 ) {
       data <- subset(data, ledgercode %in% input$ledgercode_select)
     }
-    # Private Match Waiver Filter
+    #  Filter
     if (length(input$funddescription_select)>0) {
       data <- subset(data, funddescription %in% input$funddescription_select)
     }
     return(data)
   })
   # plot three figures
-  output$plot_length <- renderPlotly({
-    dat <- iaInput()
+  output$plot1 <- renderPlotly({
+    dat <- dataInput()
     ggplotly(
-      ggplot(data= dat, aes(x = funddescription, fill = departmenttype, text = paste0("<b>"))) + 
+      ggplot(data= dat, aes(x = funddescription, fill = departmenttype)) + 
         geom_bar() + theme(axis.title.x=element_blank(),
                            axis.text.x=element_blank(),
                            axis.ticks.x=element_blank()) +
-        ggtitle("Count by Award Length") +
-        xlab("Award Length") +
-        ylab("Count"),
-      tooltip = "text")
+        ggtitle("Count by Fund description") +
+        xlab("funddescription") +
+        ylab("Count"))
   })
-  output$plot_requested <- renderPlotly({
-    dat <- iaInput() 
-    ggplotly(
-      ggplot(data= dat, aes(x = ledgercode))) + 
-        geom_point() + theme(axis.title.x=element_blank(),
-                               axis.text.x=element_blank(),
-                               axis.ticks.x=element_blank()) +
-        ggtitle("Count by Award Requested") +
-        xlab("Applicant") +
-        ylab("Award Requested")
-  })
-  output$plot_requested2 <- renderPlotly({
-    dat <- iaInput() 
+  output$plot2 <- renderPlotly({
+    dat <- dataInput() 
     ggplotly(
       ggplot(data= dat, aes(x = ledgercode, y = amount_city)) + 
+      geom_boxplot() + theme(axis.title.x=element_blank(),
+                               axis.text.x=element_blank(),
+                               axis.ticks.x=element_blank()) +
+        ggtitle("Ledgercode and Amount ") +
+        xlab("Ledgercode") +
+        ylab("Amount"))
+  })
+  output$plot3 <- renderPlotly({
+    dat <- dataInput() 
+    ggplotly(
+      ggplot(data= dat, aes(amount_city)) + 
         geom_histogram() +   theme(axis.title.x=element_blank(),
                                    axis.text.x=element_blank(),
                                    axis.ticks.x=element_blank()) +
-        ggtitle("Count by Award Requested") +
-        xlab("Award Requested") +
+        ggtitle("Count by Amount") +
+        xlab("Amount") +
         ylab("Count")) 
   })
   # Data Table
   output$table <- DT::renderDataTable({
-    data <- iaInput()
+    data <- dataInput()
     
     subset(data, select = c(departmenttype,ledgercode,amount_city,funddescription))
   })
@@ -154,7 +155,7 @@ server <- function(input, output, session = session) {
   # Download data in the datatable
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste("investing-in-innovative-applications", Sys.Date(), ".csv", sep="")
+      paste("City Wide Revenues and Expenses", Sys.Date(), ".csv", sep="")
     },
     content = function(file) {
       write.csv(swInput(), file)
@@ -163,7 +164,8 @@ server <- function(input, output, session = session) {
   # Reset Filter Data
   observeEvent(input$reset, {
     updateSelectInput(session, "ledgercode_select", selected = c("4", "5"))
-    updateSliderInput(session, "amount_select", value = c(min(data_all$amount_city, na.rm = T), max(data_all$amount_city, na.rm = T)))
+    updateSliderInput(session, "amount_select", value = c(min(data_all$amount_city, na.rm = T), 
+                                                          max(data_all$amount_city, na.rm = T)))
     updateCheckboxGroupInput(session, "funddescription_select", choices = sort(unique(data_all$funddescription)),
                              selected = c("	GENERAL FUND","SENIOR CITIZENS PROG TF"))
     showNotification("You have successfully reset the filters", type = "message")
